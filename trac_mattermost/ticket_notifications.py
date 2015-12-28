702abd01d6cc6d965c9106fcb4453053c903b7cd
Bertrand Janin <b@janin.com>
2015-12-28 11:46:04 -0500

    Use formatting styles that don't conflict.

--- trac_mattermost/ticket_notifications.py
+++ trac_mattermost/ticket_notifications.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2015-2016 Truveris, Inc. All Rights Reserved.
 # See included LICENSE file.
 
+import re
+
 from trac.core import Component
 from trac.core import implements
 from trac.ticket.api import ITicketChangeListener, TicketSystem
@@ -29,19 +31,19 @@ class TicketNotifications(Component, TracMattermostComponent):
         for k, v in old_values.items():
             # No changes occurred, this sometimes happens when the user clicks
             # on a field but doesn't change anything.
-            if v == ticket[k]:
+            if (v or "") == (ticket[k] or ""):
                 continue
 
             if not v:
-                f = u"* **{}** set to _{}_".format(field_labels[k], ticket[k])
+                f = u"- **{}** set to *{}*".format(field_labels[k], ticket[k])
             elif not ticket[k]:
-                f = u"* **{}** unset".format(field_labels[k])
+                f = u"- **{}** unset".format(field_labels[k])
             else:
                 if len(v) > 100 or len(ticket[k]) > 100:
-                    f = u"* **{}** changed".format(field_labels[k])
+                    f = u"- **{}** changed".format(field_labels[k])
                 else:
                     f = (
-                        u"* **{}** changed from _{}_ to _{}_"
+                        u"- **{}** changed from *{}* to *{}*"
                         .format(field_labels[k], v, ticket[k])
                     )
             formatted.append(f)
@@ -59,13 +61,17 @@ class TicketNotifications(Component, TracMattermostComponent):
         self.send_notification(text)
 
     def ticket_changed(self, ticket, comment, author, old_values):
-        if len(comment) > 100:
-            comment = comment[:97] + "..."
+        if comment:
+            if len(comment) > 100:
+                comment = comment[:97] + "..."
+            comment = re.sub(r"^", "> ", comment, flags=re.MULTILINE)
+        else:
+            comment = ""
 
         if old_values:
             fmt = (
                 u"@{username} changed {ticket}:\n"
-                "{changes}\n\n"
+                "{changes}\n"
                 "{comment}"
             )
         else:
