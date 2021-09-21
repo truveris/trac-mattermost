1ef006f427a36c0260a0a12e223560a3c65d99b3
Joachim Mairboeck <joachim.mairboeck@risc-software.at>
2021-09-21 11:30:52 +0200

    ticket_notifications: add ticket_deleted, ticket_comment_modified, ticket_change_deleted (partially untested)

--- trac_mattermost/ticket_notifications.py
+++ trac_mattermost/ticket_notifications.py
@@ -43,29 +43,41 @@ class TicketNotifications(Component, TracMattermostComponent):
             )
         )
 
+    def format_change(self, key, old_value, new_value, formatted):
+        # No changes occurred, this sometimes happens when the user clicks
+        # on a field but doesn't change anything.
+        if (old_value or "") == (new_value or ""):
+            return
+
+        if not old_value:
+            f = u"**{0}** set to *{1}*".format(key, new_value)
+        elif not new_value:
+            f = u"**{0}** unset".format(key)
+        else:
+            if len(old_value) > 100 or len(new_value) > 100:
+                f = u"**{0}** changed".format(key)
+            else:
+                f = (
+                    u"**{0}** changed from *{1}* to *{2}*"
+                    .format(key, old_value, new_value)
+                )
+        formatted.append(f)
+
     def format_changes(self, ticket, old_values):
         field_labels = TicketSystem(self.env).get_ticket_field_labels()
 
         formatted = []
         for k, v in old_values.items():
-            # No changes occurred, this sometimes happens when the user clicks
-            # on a field but doesn't change anything.
-            if (v or "") == (ticket[k] or ""):
-                continue
+            self.format_change(field_labels[k], v, ticket[k], formatted)
 
-            if not v:
-                f = u"**{0}** set to *{1}*".format(field_labels[k], ticket[k])
-            elif not ticket[k]:
-                f = u"**{0}** unset".format(field_labels[k])
-            else:
-                if len(v) > 100 or len(ticket[k]) > 100:
-                    f = u"**{0}** changed".format(field_labels[k])
-                else:
-                    f = (
-                        u"**{0}** changed from *{1}* to *{2}*"
-                        .format(field_labels[k], v, ticket[k])
-                    )
-            formatted.append(f)
+        return u"\n".join(formatted)
+
+    def format_old_changes(self, changes):
+        field_labels = TicketSystem(self.env).get_ticket_field_labels()
+
+        formatted = []
+        for k, (old_value, new_value) in changes.items():
+            self.format_change(field_labels[k], old_value, new_value, formatted)
 
         return u"\n".join(formatted)
 
@@ -99,6 +111,49 @@ class TicketNotifications(Component, TracMattermostComponent):
             username=author,
             comment=comment,
             changes=self.format_changes(ticket, old_values),
+        ).strip()
+
+        self.send_notification(text)
+
+    def ticket_deleted(self, ticket):
+        text = (
+            u"Ticket #{ticket_id}. {summary} deleted"
+        ).format(
+                ticket_id=ticket.id,
+                summary=ticket["summary"],
+        )
+
+        self.send_notification(text)
+
+    def ticket_comment_modified(self, ticket, cdate, author, comment, old_comment):
+        comment = format_comment(comment)
+        old_comment = format_comment(old_comment)
+
+        fmt = (
+            u"@{username} modifed comment on {ticket} from:\n"
+            "{old_comment}\n\n"
+            "to:\n"
+            "{comment}"
+        )
+
+        text = fmt.format(
+            ticket=self.format_ticket(ticket),
+            username=author,
+            comment=comment,
+            old_comment=old_comment,
+        ).strip()
+
+        self.send_notification(text)
+
+    def ticket_change_deleted(self, ticket, cdate, changes):
+        fmt = (
+            u"a change of {ticket} was deleted:\n"
+            "{changes}"
+        )
+
+        text = fmt.format(
+            ticket=self.format_ticket(ticket),
+            changes=self.format_old_changes(changes),
         ).strip()
 
         self.send_notification(text)
